36e6793eb32d9fc18d30d89447c92f9a7f63e0fe
Jorge Estebaranz Escribano <jorge241290@gmail.com>
2021-06-08 12:59:33 +0200

    Enemy COntrollet

--- ProyectoFinCurso/Assets/_Scripts/EnemyBehaviour.cs
+++ ProyectoFinCurso/Assets/_Scripts/EnemyBehaviour.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Tooltip("Velocidad de movimiento del enemigo")]
+    public float speed = 1;
+    private Rigidbody2D _rigidbody;
+
+    private bool isMoving;
+
+    [Tooltip("Tiempo que tarda el enemigo entre pasos sucesivos")]
+    public float timeBetweenSteps;
+    private float timeBetweenStepsCounter;
+
+    [Tooltip("Tiempo que tarda el enemigo en dar un paso")]
+    public float timeToMakeStep;
+    private float timeToMakeStepCounter;
+    [Tooltip("Dirección en la que se mueve el enemigo, se genera aleatoriamente")]
+    public Vector2 directionToMove;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        timeBetweenStepsCounter = timeBetweenSteps;
+        timeToMakeStepCounter = timeToMakeStep;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (isMoving){
+
+            timeToMakeStepCounter -= Time.deltaTime;
+            _rigidbody.velocity = directionToMove * speed;
+            //Cuando me quedo sin tiempo de movimiento, paramos al enemigo.
+            if (timeToMakeStepCounter < 0) {
+                isMoving = false;
+                timeBetweenStepsCounter = timeBetweenSteps;
+                _rigidbody.velocity = Vector2.zero;
+            }
+        }
+        else {
+            timeBetweenStepsCounter -= Time.deltaTime;
+            //Cuando me quedo sin tiempo de estar parado, arrancamos al enemigo para que de un paso.
+            if (timeBetweenStepsCounter < 0) {
+                isMoving = true;
+                timeToMakeStepCounter = timeToMakeStep;
+                //El movimiento del enemigo lo realizamos aleatoriamente.
+                directionToMove = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
+            }
+        }
+
+
+
     }
 }
