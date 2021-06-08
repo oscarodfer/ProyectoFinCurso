c0ec5d5ab732d50a60c0248f9e045d6baf60a47c
Jorge Estebaranz Escribano <jorge241290@gmail.com>
2021-06-08 16:20:50 +0200

    Sangre en el enemigo

--- ProyectoFinCurso/Assets/_Scripts/WeaponDamage.cs
+++ ProyectoFinCurso/Assets/_Scripts/WeaponDamage.cs
@@ -7,10 +7,22 @@ public class WeaponDamage : MonoBehaviour
     [Tooltip("Cantidad de daño que hace el arma")]
     public int damage;
 
+    public GameObject bloodAnimation;
+    private GameObject hitPoint;
+
+    private void Start()
+    {
+        hitPoint = transform.Find("Hit Point").gameObject;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Enemy")) 
         {
+            if (bloodAnimation != null && hitPoint!=null) 
+            {
+                Instantiate(bloodAnimation, hitPoint.transform.position, hitPoint.transform.rotation);
+            }
             collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
             
         }
