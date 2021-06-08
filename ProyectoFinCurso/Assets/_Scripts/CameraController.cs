7403b40c920309a9a6165a331094e4e209432555
Oscar Rodríguez <oscarodfer@gmail.com>
2021-06-08 16:27:05 +0200

    Camera and boundaries

--- ProyectoFinCurso/Assets/_Scripts/CameraController.cs
+++ ProyectoFinCurso/Assets/_Scripts/CameraController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private GameObject target;
+    private Vector3 targetPosition;
+    private float cameraSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Player");
+        cameraSpeed = target.GetComponent<PlayerController>().speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.deltaTime * cameraSpeed);
     }
 }
